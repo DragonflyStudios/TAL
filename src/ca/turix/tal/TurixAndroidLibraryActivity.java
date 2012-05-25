e904a8aba7b427420c348c9d3fda0fd64d5384cf
Jun Luo <jun@turix.ca>
2012-05-24 22:09:10 -0400

    first commit

---
+++ src/ca/turix/tal/TurixAndroidLibraryActivity.java
@@ -0,0 +1,13 @@
+package ca.turix.tal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TurixAndroidLibraryActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
